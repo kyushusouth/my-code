03d5c201b1beca490cb3a58f1898e1315bef6219
kyushusouth <90747163+kyushusouth@users.noreply.github.com>
2022-04-13 08:03:32 +0900

    add new filw

---
+++ lab/deep-learning-from-scratch-2-master/ch02/count_method_small.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+import sys
+sys.path.append('..')
+import numpy as np
+import matplotlib.pyplot as plt
+from common.util import preprocess, create_co_matrix, ppmi
+
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+vocab_size = len(id_to_word)
+C = create_co_matrix(corpus, vocab_size, window_size=1)
+W = ppmi(C)
+
+# SVD
+U, S, V = np.linalg.svd(W)
+
+np.set_printoptions(precision=3)  # 有効桁３桁で表示
+print(C[0])
+print(W[0])
+print(U[0])
+
+"""
+for word, word_id in word_to_id.items():
+    print(word)
+    print(word_id)
+    print("")
+は
+
+for word, word_id in enumerate(word_to_id):
+    print(word)
+    print(word_id)
+    print("")
+の逆
+"""
+# plot
+# Uは2次元配列であり，word_idに対応した最初の2次元の値を使ってプロット。
+for word, word_id in word_to_id.items():
+    plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
+plt.scatter(U[:,0], U[:,1], alpha=0.5)
+plt.show()
